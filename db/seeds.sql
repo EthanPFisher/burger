baa2e5381db35b5b16195aeaa4a60e974d7fd171
Loafus <eth.fish@gmail.com>
2018-11-17 12:02:18 -0800

    logic done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Eat Me Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Already Eaten Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gone Fishing Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('123 Burger', true);
